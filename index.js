b471f92c8ec2b9293f3b4af0c2c6f4322693ff5a
shovan-642 <shovan642@gmail.com>
2023-10-20 16:38:57 +0600

    chef-portfolio add

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+const chefPortfolio = require('./data/chef_portfolio.json')
+
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('Chef is running')
+
+})
+
+app.get('/chef_portfolio', (req, res)=>{
+    res.send(chefPortfolio)
+})
+
+app.listen(port, ()=>{
+    console.log(`Chef port is running ${port}`)
+})
+
+
